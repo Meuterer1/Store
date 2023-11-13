b1befd9d48738cfd9d62462bc8f9a2c40ee581a9
Meuterer1 <kamila.dziuba@o2.pl>
2023-11-13 18:57:03 +0100

    update webpack configuration

--- webpack/production.js
+++ webpack/production.js
@@ -42,6 +42,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/', // Wskazuje, gdzie mają być zapisywane pliki
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
